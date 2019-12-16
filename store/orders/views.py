91e0fa634d387aaa0ff1e74fea769491b20b8b8c
jesus <jesbaga.17@gmail.com>
2019-12-16 07:41:42 -0500

    relationship order and address

--- store/orders/views.py
+++ store/orders/views.py
@@ -17,14 +17,16 @@ def order(request):
         'breadcrumb': breadcrumb()
     })
 
+@login_required(login_url='login')
 def address(request):
     cart = get_or_create_cart(request)
     order = get_or_create_order(cart, request)
 
-    shipping_address = order.shipping_address
+    shipping_address = order.get_or_set_shipping_address()
 
     return render(request, 'orders/address.html', {
         'cart': cart,
         'order': order,
+        'shipping_address': shipping_address,
         'breadcrumb': breadcrumb(address=True)
     })
